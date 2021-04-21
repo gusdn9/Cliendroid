e8557fdfed6e53e587870108b0c7be4bcc935207
hyunwoo.lee <gusdn9@gmail.com>
2021-04-21 20:24:45 +0900

    toolbar drawer navigation 적용

--- app/src/main/java/com/hyunwoo/cliendroid/presentation/activity/main/MainActivity.kt
+++ app/src/main/java/com/hyunwoo/cliendroid/presentation/activity/main/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.hyunwoo.cliendroid.presentation.activity.main
 
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.hyunwoo.cliendroid.R
 import com.hyunwoo.cliendroid.architecture.AppActivity
+import com.hyunwoo.cliendroid.databinding.ActivityMainBinding
 
 class MainActivity : AppActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        setupDrawer(navController)
+    }
+
+    private fun setupDrawer(navController: NavController) {
+        // Root가 아닌 경우 drawer를 gesture로 열지 못하게 막음.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == navController.graph.startDestination) {
+                DrawerLayout.LOCK_MODE_UNLOCKED
+            } else {
+                DrawerLayout.LOCK_MODE_LOCKED_CLOSED
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
