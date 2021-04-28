ea85985ef7df6d4c488834497b33b2d6a47d5a2e
hyunwoo.lee <gusdn9@gmail.com>
2021-04-28 20:54:26 +0900

    검색 페이지 개발중. fragment, viewModel. keyboard event 추가 미비

--- app/src/main/java/com/hyunwoo/cliendroid/presentation/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/hyunwoo/cliendroid/presentation/fragment/search/SearchFragment.kt
@@ -4,10 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import com.airbnb.mvrx.Loading
 import com.airbnb.mvrx.fragmentViewModel
+import com.google.android.material.tabs.TabLayout
+import com.hyunwoo.cliendroid.R
 import com.hyunwoo.cliendroid.architecture.AppFragment
 import com.hyunwoo.cliendroid.common.exception.ViewBindingException
 import com.hyunwoo.cliendroid.databinding.FragmentSearchBinding
+import com.hyunwoo.cliendroid.domain.model.SearchItem
+import com.hyunwoo.cliendroid.domain.model.SearchSort
+import com.hyunwoo.cliendroid.extension.isProgressDialogVisible
 import javax.inject.Inject
 
 class SearchFragment : AppFragment() {
@@ -20,17 +32,121 @@ class SearchFragment : AppFragment() {
     @Inject
     lateinit var viewModelFactory: SearchViewModel.Factory
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    private val boardAdapter by lazy {
+        BoardSpinnerAdapter(requireContext())
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private val searchListAdapter by lazy {
+        SearchListAdapter(imageLoader, this::onSearchItemClicked)
+    }
+
+    private val tabSelectedListener = object : TabLayout.OnTabSelectedListener {
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            when (tab?.text) {
+                getString(R.string.search_sort_recency) -> {
+                    viewModel.setSort(SearchSort.RECENCY)
+                }
+                getString(R.string.search_sort_accuracy) -> {
+                    viewModel.setSort(SearchSort.ACCURACY)
+                }
+            }
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) = Unit
+
+        override fun onTabReselected(tab: TabLayout.Tab?) = Unit
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        subscribeStates()
+    }
+
+    private fun subscribeStates() {
+        viewModel.onEach(State::searchRefreshAsync) { async ->
+            binding.refreshLayout.isRefreshing = async is Loading
+        }
+
+        viewModel.onEach(State::searchLoadMoreAsync) { async ->
+            isProgressDialogVisible = async is Loading
+        }
+
+        viewModel.onEach(State::keyword, State::sort, State::sort, State::boardId) { keyword, _, _, _ ->
+            binding.searchView.setQuery(keyword, false)
+            viewModel.refresh()
+        }
+
+        viewModel.onEach(State::boardList) { boardList ->
+            binding.boardSpinner.isVisible = boardList != null
+
+            if (boardList == null) return@onEach
+            boardAdapter.clear()
+            boardAdapter.addAll(boardList)
+        }
+
+        viewModel.onEach(State::searchResultList) { searchList ->
+            searchListAdapter.submitList(searchList)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        initListeners()
+    }
+
+    private fun initViews() {
+        binding.boardSpinner.adapter = boardAdapter
+        binding.recyclerView.adapter = searchListAdapter
+        binding.recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                binding.recyclerView.context,
+                LinearLayoutManager.VERTICAL
+            )
+        )
+    }
+
+    private fun initListeners() {
+        binding.refreshLayout.setOnRefreshListener {
+            viewModel.refresh()
+        }
+
+        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (binding.recyclerView.canScrollVertically(1).not()) {
+                    viewModel.loadMore()
+                }
+            }
+        })
+
+        binding.tabLayout.addOnTabSelectedListener(tabSelectedListener)
+
+        binding.boardSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>?, selectedItemView: View?, position: Int, id: Long) {
+                viewModel.setBoardId(boardAdapter.getItem(position)?.id)
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) = Unit
+        }
+    }
+
     override fun onDestroyView() {
+        binding.tabLayout.removeOnTabSelectedListener(tabSelectedListener)
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onSearchItemClicked(searchItem: SearchItem) {
+        // TODO 상세로 이동해야함.
     }
 }
--- app/src/main/java/com/hyunwoo/cliendroid/presentation/fragment/search/SearchViewModel.kt
+++ app/src/main/java/com/hyunwoo/cliendroid/presentation/fragment/search/SearchViewModel.kt
@@ -4,17 +4,22 @@ import com.airbnb.mvrx.FragmentViewModelContext
 import com.airbnb.mvrx.Loading
 import com.airbnb.mvrx.MavericksViewModelFactory
 import com.airbnb.mvrx.Success
+import com.airbnb.mvrx.Uninitialized
 import com.airbnb.mvrx.ViewModelContext
 import com.hyunwoo.cliendroid.architecture.AppMvRxViewModel
+import com.hyunwoo.cliendroid.domain.model.SearchSort
 import com.hyunwoo.cliendroid.domain.usecase.SearchUseCase
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class SearchViewModel @AssistedInject constructor(
     @Assisted initialState: State,
     private val searchUseCase: SearchUseCase
 ) : AppMvRxViewModel<State>(initialState) {
+
+    private var loadMoreJob: Job? = null
 
     init {
         refresh()
@@ -25,16 +30,57 @@ class SearchViewModel @AssistedInject constructor(
             return@withState
         }
 
+        loadMoreJob?.cancel()
+        setState { copy(searchLoadMoreAsync = Uninitialized) }
+
         viewModelScope.launch {
-            searchUseCase::invoke.asAsync(state.keyword, state.page, state.sort, state.boardId) { async ->
+            searchUseCase::invoke.asAsync(state.keyword, 0, state.sort, state.boardId) { async ->
                 var nextState = this
                 if (async is Success) {
                     val result = async()
-                    nextState = nextState.copy(boardList = result.boardList, searchResultList = result.searchList)
+                    nextState = nextState.copy(
+                        boardList = result.boardList,
+                        searchResultList = result.searchList,
+                        page = 0
+                    )
                 }
                 nextState.copy(searchRefreshAsync = async)
             }
         }
+    }
+
+    fun loadMore() = withState { state ->
+        if (state.searchRefreshAsync is Loading || state.searchLoadMoreAsync is Loading) {
+            return@withState
+        }
+
+        val prevEntries = state.searchResultList ?: return@withState
+        loadMoreJob = viewModelScope.launch {
+            searchUseCase::invoke.asAsync(state.keyword, state.page + 1, state.sort, state.boardId) { async ->
+                var nextState = this
+                if (async is Success) {
+                    val result = async()
+                    nextState = nextState.copy(
+                        searchResultList = prevEntries + result.searchList,
+                        page = state.page + 1
+                    )
+                }
+                nextState.copy(searchLoadMoreAsync = async)
+            }
+        }
+    }
+
+    fun setSort(selectedSort: SearchSort) = setState {
+        copy(sort = selectedSort, page = 0)
+    }
+
+    fun setBoardId(boardId: String?) = setState {
+        val id = if (boardId.isNullOrEmpty()) {
+            null
+        } else {
+            boardId
+        }
+        copy(boardId = id)
     }
 
     @AssistedInject.Factory
